0fe967811eb95542808186d8ed78822b539f6dbf
Aaron Russo <aaronrusso@hotmail.it>
2019-12-05 01:04:24 +0100

    Added 3.8 requirement

--- setup.py
+++ setup.py
@@ -16,4 +16,9 @@ info = {
 }
 
 if __name__ == '__main__':
-    setup(**info)
+    import sys
+    if sys.version_info < (3,8):
+        sys.exit('Sorry, Python < 3.8 is not supported,'
+                 + ' please update to install.')
+    else:
+        setup(**info)
