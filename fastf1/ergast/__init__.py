bbd73044f43d94443ea6d49f1cb0bb3f73329adb
theOehrly <phi.schaefer@mailbox.org>
2023-04-04 21:42:42 +0200

    remove base url from __init__

--- fastf1/ergast/__init__.py
+++ fastf1/ergast/__init__.py
@@ -1,14 +1,6 @@
-import fastf1.ergast.interface
-
 # imports for exposed names
 from fastf1.ergast.interface import Ergast  # noqa: F401
 from fastf1.ergast.legacy import \
     fetch_day, \
     fetch_season, \
     fetch_results  # noqa: F401
-
-
-@property
-def base_url():
-    # TODO warn
-    return fastf1.ergast.interface.BASE_URL
