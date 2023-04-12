22ec642da286ae7fa2645f474fa47d21f0d593a2
Xuanyi Wang <wangxuanyi10@gmail.com>
2023-04-12 23:28:19 +0100

    doc path clean up

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sys
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../'))
 import os.path
 from datetime import datetime
 import re
