86ada2c343b02dce4a2dfc6e36db6e81eee4da34
Rafael <58039191+erdieee@users.noreply.github.com>
2023-07-22 09:17:12 +0200

    FIX: asyncio.run incorrectly called twice in SignalR client example

--- examples/example_fastf1_signalrclient.py
+++ examples/example_fastf1_signalrclient.py
@@ -5,11 +5,10 @@ Demonstrates the usage of the SignalRClient
 """
 from fastf1.livetiming.client import SignalRClient
 
-import asyncio
 import logging
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 client = SignalRClient(filename="output.txt", debug=True)
-asyncio.run(client.start())
+client.start()
