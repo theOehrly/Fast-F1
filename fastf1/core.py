9f1265c0b2fa14396d2ec54fa834bf42c5f1b2b6
theOehrly <23384863+theOehrly@users.noreply.github.com>
2025-01-18 16:44:26 +0100

    fix line length

--- fastf1/core.py
+++ fastf1/core.py
@@ -1551,7 +1551,8 @@ class Session:
                 laps_start_time.insert(0, self.session_start_time)
             else:
                 laps_start_time.insert(0, pd.NaT)
-            laps_start_time = pd.Series(laps_start_time, dtype="timedelta64[ns]")
+            laps_start_time = pd.Series(laps_start_time,
+                                        dtype="timedelta64[ns]")
 
             # don't set lap start times after red flag restart to the time
             # at which the previous lap was set
