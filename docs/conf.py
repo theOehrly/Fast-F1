088dc6a95d04750521831e4617820c6496a68af7
Philipp Schaefer <23384863+theOehrly@users.noreply.github.com>
2025-03-13 14:52:47 +0100

    DOC: Switch to Sphinx 8.0, fix warnings and use new logo (#705)
    * MNT: use sphinx 8
    
    * MNT: fix doc summary warnings
    
    * DOC: add new logo
    
    * remove separate doc build requirements from test/dev requirements

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,7 @@ nitpick_ignore_files = [
 html_theme = 'furo'
 html_theme_options = {
 }
+html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
