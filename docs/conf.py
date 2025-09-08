714d09890d53ed89a123728dd3a071651a03670d
Philipp Schaefer <23384863+theoehrly@users.noreply.github.com>
2025-09-08 23:49:34 +0200

    remove no longer needed signalr_aio submodule

--- docs/conf.py
+++ docs/conf.py
@@ -84,9 +84,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    '../fastf1/signalr_aio'
-]
+exclude_patterns = []
 
 nitpicky = True
 
--- fastf1/signalr_aio/events/__init__.py
+++
@@ -1,1 +0,0 @@
-from ._events import EventHook
