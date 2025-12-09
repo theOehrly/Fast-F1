964ae5c3c47d03e7ee4ac683c29192e5ea2f5758
Yatharth Saroha <icyxarse@gmail.com>
2025-12-09 16:01:29 +0530

    Update __main__.py

--- fastf1/__main__.py
+++ fastf1/__main__.py
@@ -1,4 +1,19 @@
 import argparse
+from random import randint
+from asciimatics.screen import Screen
+
+def demo(screen):
+    while True:
+        screen.print_at('Hello world!',
+                        randint(0, screen.width), randint(0, screen.height),
+                        colour=randint(0, screen.colours - 1),
+                        bg=randint(0, screen.colours - 1))
+        ev = screen.get_key()
+        if ev in (ord('Q'), ord('q')):
+            return
+        screen.refresh()
+
+Screen.wrapper(demo)
 
 from fastf1.internals.f1auth import (
     clear_auth_token,
