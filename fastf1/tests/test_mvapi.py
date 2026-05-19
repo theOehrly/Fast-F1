c8f566c88c6b30d0992e7133d84f874ebf06460f
Philipp Schaefer <23384863+theoehrly@users.noreply.github.com>
2026-05-20 00:04:23 +0200

    TST: add option to cache error responses; fix mvapi test

--- fastf1/tests/test_mvapi.py
+++ fastf1/tests/test_mvapi.py
@@ -1,7 +1,12 @@
 import requests_mock
 
+import fastf1
 from fastf1 import get_session
 from fastf1.mvapi import get_circuit_info
+from fastf1.testing import (
+    capture_log,
+    run_in_subprocess
+)
 
 
 def _setup_mocker(mocker):
@@ -37,6 +42,21 @@ def test_get_circuit_info_warns_no_telemetry(caplog):
     assert "Failed to generate marker distance information" in caplog.text
 
 
-def test_get_circuit_info_invalid_key(caplog):
+def test_get_circuit_info_invalid_key():
+    run_in_subprocess(_test_get_circuit_info,
+                      patch_cache_error_responses=True)
+
+
+def _test_get_circuit_info():
+    # requires a subprocess to prevent the modification of cache settings from
+    # influencing other tests
+    cache_settings = fastf1.Cache._requests_session_cached.settings
+
+    # cache expected error response so that tests can be run offline
+    cache_settings.allowable_codes = (200, 404, 504)
+    cache_settings.cache_control = False
+
+    log_handle = capture_log()
+
     get_circuit_info(year=2020, circuit_key=0)
-    assert "Failed to load circuit info" in caplog.text
+    assert "Failed to load circuit info" in log_handle.text
