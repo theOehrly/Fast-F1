bcb6f0499b8b79eafe4786726f141b14304cd806
Philipp Schaefer <23384863+theoehrly@users.noreply.github.com>
2026-07-02 23:48:16 +0200

    TST: cleanup tests for new infrastructure, remove old and unused tests and data

--- fastf1/tests/test_mvapi.py
+++ fastf1/tests/test_mvapi.py
@@ -6,18 +6,8 @@ from fastf1.testing import (
 )
 
 
-def _setup_mocker(mocker):
-    with open('fastf1/testing/reference_data/2020_05_FP2/mvapi_circuits.raw',
-              'rb') as fobj:
-        content = fobj.read()
-    mocker.get('https://api.multiviewer.app/api/v1/circuits/39/2020',
-               content=content, status_code=200)
-
-
 def test_get_circuit_info():
-    with requests_mock.Mocker() as mocker:
-        _setup_mocker(mocker)
-        circuit_info = get_circuit_info(year=2020, circuit_key=39)
+    circuit_info = get_circuit_info(year=2020, circuit_key=39)
 
     assert circuit_info is not None
 
@@ -32,9 +22,7 @@ def test_get_circuit_info_warns_no_telemetry(caplog):
     session = get_session(2020, 'Italy', 'R')
     session.load(telemetry=False)
 
-    with requests_mock.Mocker() as mocker:
-        _setup_mocker(mocker)
-        session.get_circuit_info()
+    session.get_circuit_info()
 
     assert "Failed to generate marker distance information" in caplog.text
 
