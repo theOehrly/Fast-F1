theOehrly/Fast-F1

commit 40d32ff23378ba2c0a6be1c3bb811414af298ae8

FIX: Improve robustness and error handling of driver-team-mapping generation (#954)

fastf1/plotting/_backend.py
@@ -58,13 +58,16 @@ def _load_drivers_from_f1_livetiming(
     #   primary style
     for num in sorted(driver_info.keys()):
         driver_entry = driver_info[num]
-        team_name = driver_entry.get("TeamName")
-        team_color = f"#{driver_entry.get('TeamColour').lower()}"
+        team_name = driver_entry.get("TeamName", "")
+
+        team_color = driver_entry.get("TeamColor", "").lower()
+        team_color = f"#{team_color}" if team_color else ""
+
         abbreviation = driver_entry.get("Tla", "")
         name = " ".join((driver_entry.get("FirstName", ""),
                          driver_entry.get("LastName", "")))
 
-        if not abbreviation.strip() or not name.strip():
+        if not (abbreviation.strip() and name.strip() and team_name.strip()):
             _logger.warning(
                 "Skipping driver with incomplete data while generating "
                 "driver-team mapping for plotting constants."
@@ -76,12 +79,17 @@ def _load_drivers_from_f1_livetiming(
         for normalized_name, team in teams.items():
             if normalized_name in normalized_full_team_name:
                 team.name = team_name
-                team.colors.official = team_color
+                if team_color:
+                    # only update if the API provided a color value
+                    team.colors.official = team_color
                 break
         else:
             team = _generate_team(team_name, team_color)
 
             _logger.warning(f"Auto-generating unknown team: {team.name}")
+            if not team_color:
+                _logger.warning(f"Auto-generated team {team.name} has no "
+                                f"team color.")
 
             teams[team.normalized_name] = team
 
@@ -94,7 +102,10 @@ def _load_drivers_from_f1_livetiming(
         team.add_driver(driver)
 
     # return all teams that have drivers in this session
-    return [team for team in teams.values() if team.drivers]
+    teams = [team for team in teams.values() if team.drivers]
+    if not teams:
+        _logger.warning("Failed to generate driver-team mapping!")
+    return teams
 
 
 def _generate_team(team_name: str, team_color: str) -> Team:

fastf1/plotting/_interface.py
@@ -43,7 +43,10 @@ def _get_driver_team_mapping(
         mapping = DriverTeamMapping(year=year, teams=teams)
         _DRIVER_TEAM_MAPPINGS[api_path] = mapping
 
-    return _DRIVER_TEAM_MAPPINGS[api_path]
+    dtm = _DRIVER_TEAM_MAPPINGS[api_path]
+    if not dtm.teams:
+        raise ValueError("No drivers/teams found for this session!")
+    return dtm
 
 
 def _get_driver(

commit 5b7a6291e096b14e726e05c9d0cf686ea1e44b39

FIX: correct typo introduced in #954

fastf1/plotting/_backend.py
@@ -60,7 +60,7 @@ def _load_drivers_from_f1_livetiming(
         driver_entry = driver_info[num]
         team_name = driver_entry.get("TeamName", "")
 
-        team_color = driver_entry.get("TeamColor", "").lower()
+        team_color = driver_entry.get("TeamColour", "").lower()
         team_color = f"#{team_color}" if team_color else ""
 
         abbreviation = driver_entry.get("Tla", "")